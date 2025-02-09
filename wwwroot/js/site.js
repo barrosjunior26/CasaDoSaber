abc2047b6451749bf5641a0c2c4bd8d040b05781
barrosjunior26 <barrosjunior26@outlook.com.br>
2025-02-09 13:53:59 -0300

    Quinto commit - Inclusão dos id nas tags HTML para para linkar com o código JS e calcular a média do aluno. Foi implementado a lógica no arquivo JS para calcular a média do aluno e apresenta-lá em tempo real.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,4 +6,42 @@ setTimeout(function () {
     $(".alert").fadeOut("slow", function () {
         $(this).alert("close");
     });
-},5000);
+}, 5000);
+
+$(document).ready(function () {
+    function calcularMedia() {
+        // Obtém os valores dos campos de entrada
+        var ciencias = parseFloat(document.getElementById("ciencias").value) || 0;
+        var geografia = parseFloat(document.getElementById("geografia").value) || 0;
+        var estrangeira = parseFloat(document.getElementById("estrangeira").value) || 0;
+        var matematica = parseFloat(document.getElementById("matematica").value) || 0;
+        var portugues = parseFloat(document.getElementById("portugues").value) || 0;
+        var mediaColor = window.document.getElementById("media");
+
+        // Calcula a média
+        var media = (ciencias + geografia + estrangeira + matematica + portugues) / 5;
+
+        // Atualiza o campo de média
+        document.getElementById("media").value = media.toFixed(2);
+
+        mediaColor.value = media.toFixed(2);
+
+        if (media >= 7) {
+            mediaColor.style.color = "green";
+            mediaColor.style.fontWeight = "800";
+        } else if (media >= 1 && media <= 6.99)
+        {
+            mediaColor.style.color = "red";
+            mediaColor.style.fontWeight = "800";
+        }else {
+            mediaColor.style.color = "black";
+            mediaColor.style.fontWeight = "800";
+        }
+    }
+
+    // Adiciona evento 'input' a cada campo para recalcular a média em tempo real
+    $("#ciencias, #geografia, #estrangeira, #matematica, #portugues").on("input", calcularMedia);
+
+    // Calcula a média inicialmente
+    calcularMedia();
+});
